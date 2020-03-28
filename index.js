a1ffcb8384a177ae259b94eb814759229846e1b5
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-28 19:27:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,6 +73,13 @@ function removeFromCart(item) {
     return "That item is not in your cart."
 }
 
-function placeOrder(cardNumber) {
-  // write your code here
+function placeOrder(cardNum){
+  if(placeOrder()){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    for(var i = 0; i < getCart().length; i++) {
+        cart.splice(i, 1);
+    }
+    return "Your total cost is $${total()}, which will be charged to the card ${cardNum}."
+  }
 }
